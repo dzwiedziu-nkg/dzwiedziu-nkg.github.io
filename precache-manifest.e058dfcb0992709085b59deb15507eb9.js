0b550e4e1eb9348d64bf24eed0ba3a24fe511ba1
nkg <nkg753@gmail.com>
2018-12-12 18:04:28 +0100

    New version

---
+++ precache-manifest.e058dfcb0992709085b59deb15507eb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e9a27e5e10693e7107c7",
+    "url": "/static/js/main.e9a27e5e.chunk.js"
+  },
+  {
+    "revision": "c9b0eaeba3a2350ae0a6",
+    "url": "/static/js/1.c9b0eaeb.chunk.js"
+  },
+  {
+    "revision": "e9a27e5e10693e7107c7",
+    "url": "/static/css/main.a078c27b.chunk.css"
+  },
+  {
+    "revision": "f0de02f37072d5b27fd5b25ca1e206e8",
+    "url": "/index.html"
+  }
+];
